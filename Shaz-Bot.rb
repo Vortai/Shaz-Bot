0972bcd26c1ba93b2188c331017e2188e11bad32
vortai <vortai.vae@gmail.com>
2015-12-23 12:46:01 +1100

    Added a basic in mumble control system
    Shaz-Bot now connects to the mumble server in question and will wait to
    recive a command before acting. However at the moment any command it
    recives will be taken as request to sort teams. Also a alternative sort,
    currently a prototype has been added and the bot now prevents move
    errors by refusing to sort less than 14 players.

--- Shaz-Bot.rb
+++ Shaz-Bot.rb
@@ -19,41 +19,43 @@ class Shaz_Bot
 		@cli = Mumble::Client.new(CONFIG['address'], CONFIG['port']) do |conf| # This segment inherits the bots basic data from the config.yml file
 			conf.username = CONFIG['username']
 			conf.password = CONFIG['password'] if CONFIG['password']
-		end			
-		@roleList = {:role1 => :hof, :role2 => :cap, :role3 => :ld, :role4 => :snipe, :role5 => :offence, :role6 => :offence, :role7 => :offence} # the role's to be picked
+		end
+		@roleList = CONFIG['role_List']
+		userInput
 	end
 
 	def hashYmlUpdate
 		players = YAML.load_file('HashStore.yml')
-        for i in 0..@cli.users.count-1 do
-            usrkey = @cli.users.keys[i]
-            hshNm = @cli.users[usrkey].hash
-            if players.include?(:"#{hshNm}") == true then
-                puts "the players current name is #{@cli.users[usrkey].name} and the channel id is #{@cli.users[usrkey].channel_id}"
-                players[:"#{hshNm}"][:name] = @cli.users[usrkey].name
-                total = 0
-                for i in 0..6 do
-                    roleName = players[:"#{hshNm}"][:playerSkills].keys[i]
-                    total = total + players[:"#{hshNm}"][:playerSkills][:"#{roleName}"]
-                end
-                players[:"#{hshNm}"].merge!(:skillTotal => total)
-            else
-                defautPlayerSkills = {:hof => 2.0,:ld => 2.0,:gamesense => 3.0,:cap => 2.0,:fragging => 3.0,:offence => 4.0,:snipe => 1.0}
-                tmpH = {:name => @cli.users[usrkey].name,:hash => @cli.users[usrkey].hash}
-                tmpH.merge!(:playerSkills => defautPlayerSkills)
-                pp (tmpH)
-                players.merge!(:"#{hshNm}" => tmpH)
-            end #make the feature that puts the usr into a hash
-        end
-        File.open('HashStore.yml','w') {|f1| f1.write(players.to_yaml)} #here commit all those hashes to the players{} hash and save that to the hash file.
-    end	
+		for i in 0..@cli.users.count-1 do
+			usrkey = @cli.users.keys[i]
+			hshNm = @cli.users[usrkey].hash
+			if players.include?(:"#{hshNm}") == true then
+				puts "the players current name is #{@cli.users[usrkey].name} and the channel id is #{@cli.users[usrkey].channel_id}"
+				players[:"#{hshNm}"][:name] = @cli.users[usrkey].name
+				total = 0
+				for i in 0..6 do
+					roleName = players[:"#{hshNm}"][:playerSkills].keys[i]
+					total = total + players[:"#{hshNm}"][:playerSkills][:"#{roleName}"]
+				end
+				players[:"#{hshNm}"].merge!(:skillTotal => total)
+			else
+				defautPlayerSkills = {:hof => 2.0,:ld => 2.0,:cap => 2.0,:offence => 4.0,:snipe => 1.0,:gamesense => 3.0,:fragging => 3.0}
+				tmpH = {:name => @cli.users[usrkey].name,:hash => @cli.users[usrkey].hash}
+				tmpH.merge!(:playerSkills => defautPlayerSkills)
+				pp (tmpH)
+				players.merge!(:"#{hshNm}" => tmpH)
+			end #make the feature that puts the usr into a hash
+		end
+		File.open('HashStore.yml','w') {|f1| f1.write(players.to_yaml)} #here commit all those hashes to the players{} hash and save that to the hash file.
+	end
 
 	def onlinePlayers # Gets a hash list of all online players and their position.
 		onPlayers = {}
 		for i in 0..@cli.users.count-1 do
 			usrkey = @cli.users.keys[i]
 			hshNm = @cli.users[usrkey].hash
-			if @cli.users[usrkey].channel_id == CONFIG['priority_players']['normal'] or @cli.users[usrkey].channel_id == CONFIG['priority_players']['quiet'] then				onPlayers.merge!(:"#{hshNm}" => true) # The key is used to store player hash data and the true/false stores fat data for said player
+			if @cli.users[usrkey].channel_id == CONFIG['priority_players']['normal'] or @cli.users[usrkey].channel_id == CONFIG['priority_players']['quiet'] then
+				onPlayers.merge!(:"#{hshNm}" => true) # The key is used to store player hash data and the true/false stores fat data for said player
 			elsif @cli.users[usrkey].channel_id == CONFIG['waiting_players']['normal'] or @cli.users[usrkey].channel_id == CONFIG['waiting_players']['quiet'] then
 				onPlayers.merge!(:"#{hshNm}" => false) # As above except false for not fat
 			end
@@ -73,6 +75,10 @@ class Shaz_Bot
 		teamDS = {}
 		playeryml = YAML.load_file('HashStore.yml')
 		onlinePlayerList = onlinePlayers()
+		if onlinePlayerList.length < 14 then
+			puts "not enough players to move"
+			return
+		end
 		for i in 0..@roleList.length-1 do
 			playerSelected = simpleSelect(CONFIG['team1']['name'], @roleList[:"role#{i+1}"], 3, onlinePlayerList)
 			pp playerSelected
@@ -113,11 +119,26 @@ class Shaz_Bot
 			end
 		end
 	end
+
+	def relativeSkillSelect(team, severity, playerlist) # A sort to select players only by skill, not role, just an idea atm.
+		skillyml = {}
+		playerDataList = YAML.load_file('HashStore.yml')
+		for i in 0..playerlist.length-1 do
+			skillyml[:"#{playerlist.keys[i]}"] = (playerDataList[:"#{playerlist.keys[i]}"][:playerSkills][:gamesense]*1.3+playerDataList[:"#{playerlist.keys[i]}"][:playerSkills][:fragging]*10).round
+		end
+	end
+
+	def userInput
+		@cli.on_text_message do |msg|
+			puts msg.message
+			hashYmlUpdate
+			teamCompositionGet
+		end
+	end
 end
 
 shazbot = Shaz_Bot.new
 shazbot.cli.connect
-sleep(2)
-shazbot.hashYmlUpdate
-shazbot.hashYmlUpdate
-shazbot.teamCompositionGet
+loop do
+	sleep(1)
+end
